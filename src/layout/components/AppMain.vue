42d44a3e255448c4e5265f5a41efc75d0845b24a
cwsya <cwsya@qq.com>
2025-01-16 19:06:46 +0800

    整合tag

--- src/layout/components/AppMain.vue
+++ src/layout/components/AppMain.vue
@@ -1,17 +1,22 @@
 <template>
   <section class="app-main">
     <transition name="fade-transform" mode="out-in">
-      <router-view :key="key" />
+      <!-- <router-view :key="key" />-->
+      <keep-alive :include="cachedViews">
+        <router-view :key="key" />
+      </keep-alive>
     </transition>
   </section>
 </template>
-
 <script>
 export default {
   name: 'AppMain',
   computed: {
     key() {
       return this.$route.path
+    },
+    cachedViews() {
+      return this.$store.state.tagsView.cachedViews
     }
   }
 }
